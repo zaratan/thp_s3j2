bf9f931b212effaa08781f7d5291ecf56e785a86
Denis Zaratan Pasin <zaratan@hey.com>
2021-07-14 05:40:50 +0900

    Day trader

---
+++ lib/s3j2/day_trader.rb
@@ -0,0 +1,29 @@
+module S3J2
+  module DayTrader
+    Roi = Struct.new(:roi, :days)
+
+    # It returns the best possible gain given the coming day values.
+    # @param [Array] day_values
+    # @return [Integer] Max gain
+    def self.day_trader(options)
+      if !options.is_a?(Array) || options.size < 2
+        raise ArgumentError,
+              "Option format isn't right. It should be an array with at least 2 elements."
+      end
+      rois(options).max_by(&:roi).days
+    end
+
+    def self.rois(day_prices)
+      day_prices.map.with_index do |current_day, index|
+        roi_day(day_prices.slice(index..), current_day, index)
+      end
+    end
+
+    def self.roi_day(day_prices, current_day, index)
+      max = day_prices.max
+      max_index = day_prices.index(max)
+      roi = max - current_day
+      Roi.new(roi, [index, max_index + index])
+    end
+  end
+end
---
+++ spec/s3j2/day_trader_spec.rb
@@ -0,0 +1,21 @@
+describe S3J2::DayTrader do
+  let(:options) { [17, 3, 6, 9, 15, 8, 6, 1, 10] }
+
+  subject(:day_trader) { described_class.day_trader(options) }
+
+  context 'when the options are empty' do
+    it 'raise an error' do
+      expect { described_class.day_trader([]) }.to raise_error(ArgumentError)
+    end
+  end
+
+  context 'when the options have only one element' do
+    it 'raise an error' do
+      expect { described_class.day_trader([1]) }.to raise_error(ArgumentError)
+    end
+  end
+
+  it 'work as expected' do
+    expect(day_trader).to eq([1, 4])
+  end
+end
